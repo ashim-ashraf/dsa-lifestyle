03fe1c315e84e87f4139c9917b45b2a55c158517
ashim-ashraf <ashimashraf@gmail.com>
2023-05-31 10:00:56 +0530

    string operations

---
+++ string/smallest.js
@@ -0,0 +1,27 @@
+arr = [3,5,4,7,8,6]
+let n = arr.length; 
+let largest =arr[0];
+let smallest = arr[0]
+
+
+for(let i = 0 ; i<n ; i++){
+  if(largest<arr[i]){
+    largest = arr[i]
+  }
+  if(smallest>arr[i]){
+    smallest = arr[i]
+  }
+}
+
+console.log(smallest,largest)
+
+
+
+let str="hello"
+let newstr=""
+
+newstr+=str[str.length-1]
+for(i=0;i<str.length-1;i++){
+    newstr+=str[i]
+}
+console.log(newstr);
---
+++ string/string-manipulation.js
@@ -0,0 +1,104 @@
+//array
+// test=[1,2,3]
+// newarr=[]
+// for(i=0;i<test.length;i++){
+//     newarr[i]=test[i]
+//     newarr[i+test.length]=test[i]
+// }
+   
+// console.log(newarr);
+
+
+function encode(string,inc) {
+    let key = inc % 26
+    let length = string.length
+    let str = ''
+    let i=0
+    while (i < length) {
+        let letterposition = string.charCodeAt(i) + key
+        124
+       
+        if (letterposition <= 122) {
+            str+=String.fromCharCode(letterposition)
+        } else {
+            str+=String.fromCharCode(96+(letterposition%122))
+        }
+        i++
+    }
+
+    return str;
+   
+}
+
+function decode(string,dec){
+    let key = dec % 26 
+    let length = string.length;
+    let str = ''
+    let i=0 
+    while (i< length){
+        let ascii = string.charCodeAt(i) - key 
+        if(ascii >= 97) {
+            str += String.fromCharCode(ascii)
+        } else {
+            str+= String.fromCharCode(122-(ascii%97))
+        }
+        i++
+    }
+    console.log(str)
+}
+
+
+let str = 'fedba'
+let code = encode(str,2)
+console.log(code);
+// decode(code,2)
+
+
+// function findduplicate(string) {
+//     let length = string.length
+//     let i = 0
+//     let arr = []
+//     let j=1
+   
+//     while (i < length) {
+//         j = i + 1
+//         while (j < length) {
+//             if (string[i] == string[j]) {
+//                 console.log(string[i]);  
+//             }
+//             j++
+//         }
+//         i++
+//     }
+// }
+
+
+// function removealphabet(str, alphabet) {
+//     let remove=''
+//     let length = str.length
+//     let i=0
+ 
+//     while (i < length-1) {
+//         if (str[i] != alphabet) {
+//             remove = remove + str[i]
+//             console.log(remove);
+           
+//         }
+//         i++
+//     }
+
+//     console.log(remove);
+   
+   
+// }
+
+
+// function reversestring(str) {
+//     let i = str.length-1
+//     let reverse = ''
+//     while (i >= 0) {
+//         reverse+=str[i]
+//         i--
+//     }
+//     console.log(reverse);
+// }
